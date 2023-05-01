343bc95fede36f6a9539611b680b7bea8ddba58f
Shakya Majumdar <shakyamajumdar1@gmail.com>
2023-05-02 00:26:41 +0530

    set up env variables

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
+require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser');
 const Pool = require('pg').Pool
 
 const app = express()
 const pool = new Pool({
-  user: '',
-  host: '',
-  database: '',
-  password: '',
-  port: 0,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 })
-const port = 3000
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -18,7 +19,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get('/', (request, response) => {
   response.sendFile('static/home.html', {root: __dirname})
 })
-
+app.get('/users', (request, response) => {
+  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+})
 app.post('/register', (request, response) => {
   let name = request.body.name;
   console.log(`name: ${name}`);
